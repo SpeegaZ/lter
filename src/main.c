5ce41a53554147978fe3ecc0eb7fb6661dfdad05
SpeegaZ <vaayuunextdev@gmail.com>
2025-09-24 19:03:36 +0530

    added basic functions

--- src/main.c
+++ src/main.c
@@ -6,9 +6,25 @@
 #include <errno.h>
 #include <termios.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+
+
+/* ----- defines ----- */
+
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 
 /* ----- data ------ */
+
+// Stores the global state of the editor
+struct editorConfig {
+	int screenrows;
+	int screencols;
+
+	struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 // stores terminal attributes
 struct termios orig_termios;
@@ -18,6 +34,9 @@ struct termios orig_termios;
 
 void die(const char* s)
 {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -26,17 +45,16 @@ void die(const char* s)
 void disableRawMode()
 {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
-	die("tcsetattr");
+		die("tcsetattr");
 }
 
 void enableRawMode()
 {
     // reads terminal attrs and stores in struct 'orig_termios'
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
-	die("tcgetattr");
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
 
     // disables term input flags
     // IXON disables system control flow keys
@@ -62,12 +80,98 @@ void enableRawMode()
 
     // applies changes done
     // TCSAFLUSH does not let more input to be taken after quitting
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
-	die("tcsetattr");
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+}
+
+// Waits for a keypress and return it
+char editorReadKey() 
+{
+	int nread;
+	char c;
+	while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+	{
+		if (nread == -1 && errno != EAGAIN) die("read");
+	}
+	return c;
+}
+
+// Gets the window size
+int getWindowSize(int *rows, int *cols)
+{
+	struct winsize ws;
+
+	// Uses TIOCGWINSZ request (Terminal Input/Output/Control Get WINdow SiZe)
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+	{
+		return -1;
+	}
+	else
+	{
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
+
+
+/* ----- output ----- */
+
+// Draws the '~' thing like vim
+// Actually called tildes
+void editorDrawRows()
+{
+	int y;
+	for (y = 0; y < 24; y++)
+	{
+		write(STDOUT_FILENO, "~\r\n", 3);
+	}
+}
+
+// Clears the screen
+void editorRefreshScreen() 
+{
+	// \x1b(27) is an escape character
+	// [2J -> J = erase in display 
+	// 	   -> 2 = clears the entire screen
+	// The escape sequence is of total 4 bytes
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	// [H -> H = positions the cursor
+	// 	  -> 1;1 = h;w
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+	editorDrawRows();
+
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+
+/* ----- input ----- */
+
+// Waits for a keypress then handles it 
+void editorProcessKeypress() 
+{
+	char c = editorReadKey();
+
+	switch (c) 
+	{
+		// CTRL+q to exit
+		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
+
+			exit(0);
+			break;
+	}
 }
 
 
 /* ----- init ----- */
+
+// Initializes all the fields in struct E
+void initEditor()
+{
+	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
 
 int main()
 {
@@ -79,23 +183,8 @@ int main()
     // pressing 'q' quits
     while (1)
     {
-		char c = '\0';
-
-		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-	    	die("read");
-
-		// checks if 'c' is ctrl chars
-		if(iscntrl(c))
-		{
-	    	printf("%d\n", c);
-		}
-		// prints ASCII codes and char
-		else
-		{
-	    	printf("%d ('%c')\r\n", c, c);
-		}
-
-		if ( c == 'q') break;
+		editorRefreshScreen();
+		editorProcessKeypress();
 	}
 
     return 0;
